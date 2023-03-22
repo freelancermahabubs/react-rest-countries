47388a8afbb86fe2c0a9381b24c4c92bd6bf7e9c
MAHABUB <info.freelancermahabub@gmail.com>
2023-03-22 15:36:02 +0600

    first commit

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css';
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(()=>{
+    fetch(`https://restcountries.com/v3.1/all`)
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello From Countries DH: {countries.length}</h1>
+    {/*   {
+        countries.map(country => console.log(country))
+      } */}
+      
+      <div className='countries-container'>
+      {
+        countries.map(country => <Country
+        key ={country.cca3}
+        country = {country}
+          ></Country>)
+      }
+      </div>
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css'
+const Country = (props) => {
+ const {name, flags, population, area, region} = props.country;
+  return (
+    <div className='country'>
+      <img className='images' src={flags.svg} alt="" />
+      <h2>Country Name: {name.common}</h2>
+      <h2>Population: {population}</h2>
+      <p><small>Area: {area}</small></p>
+      <p>Region: {region}</p>
+      
+    </div>
+  );
+};
+
+export default Country;
